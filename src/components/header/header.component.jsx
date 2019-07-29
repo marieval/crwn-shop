eddde8b738103275d2c04672cf891e23ddba806c
marieval <majiku@seznam.cz>
2019-07-29 16:01:54 +0200

    update shop-data with new data, update cart-utils to group items in cart-reducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -23,9 +23,7 @@ const Header = ({ currentUser, hidden }) => (
         CONTACT
       </Link>
       {currentUser ? (
-        <div className="option" onClick={() => auth.signOut()}>
-          SIGN OUT
-        </div>
+        <div className="option" onClick={() => auth.signOut()} />
       ) : (
         <Link className="option" to="/signin">
           SIGN IN
